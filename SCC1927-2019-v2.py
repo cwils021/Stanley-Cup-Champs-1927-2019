9e576d23933d927d953e7fe962aabf25e294c446
Chris Wilson <chrisgwilson678@gmail.com>
2019-07-10 00:36:24 -0400

    add new version,figures

---
+++ SCC1927-2019-v2.py
@@ -0,0 +1,224 @@
+import pandas as pd
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv("C:/Users/chris/Documents/GitHub Repos/Stanley-Cup-Champs-1927-2019/_data/SCC1927-2019.csv",
+                   header=0)
+
+# Pandas options for better viewing of data frame
+
+pd.set_option('max_rows', 100) # set max rows printed to 10
+pd.set_option('expand_frame_repr', False)  # expand frame by row width
+# truncate data frame when rows > max rows above
+pd.set_option('large_repr', 'truncate')
+
+data = data.set_index('Year')
+data = data.drop(2005, axis= 0)
+data = data.rename(columns={"Winning team": "team_win",
+                            "Coach": "coach_win",
+                            "Losing team": "team_lose",
+                            "Coach.1": "coach_lose",
+                            "Winning goal\n": "win_gs"
+                            })
+
+#print(data)
+
+#split record and appearances from win_team
+data[['team_win','DROP','WREC']] = data.team_win.str.rpartition('(')
+data = data.drop('DROP', axis=1)
+data['team_win'] = data.team_win.str.rstrip()
+
+#create temp df to split conf from win_team
+temp = data.team_win.str.rsplit("(",1).to_frame()
+temp[['win_team','wcon']] = pd.DataFrame(temp.team_win.values.tolist(),index=temp.index)
+temp = temp.drop('team_win',axis=1)
+
+#print(temp)
+
+#add team_Win and wcon to df
+data[['team_win','wcon']] = temp[['win_team','wcon']]
+
+data['wcon'] = data.wcon.str.rstrip(')')
+data['wcon'] = data.wcon.str.lstrip()
+
+#clean WREC col by splitting apperances to date and record to date
+data[['wt_app2d','DROP','wt_rec2d']] = data.WREC.str.partition(',')
+data = data.drop(['WREC','DROP'],axis=1)
+data['wt_rec2d'] = data.wt_rec2d.str.rstrip(')')
+
+
+#split record and appearances from lose_team
+data[['team_lose','DROP','LREC']] = data.team_lose.str.rpartition('(')
+data = data.drop('DROP', axis=1)
+data['team_lose'] = data.team_lose.str.rstrip()
+
+#create temp df to split conf from lose_team
+temp = data.team_lose.str.rsplit("(",1).to_frame()
+temp[['lose_team','lcon']] = pd.DataFrame(temp.team_lose.values.tolist(),index=temp.index)
+temp = temp.drop('team_lose',axis=1)
+
+#add team_lose and lcon to df
+data[['team_lose','lcon']] = temp[['lose_team','lcon']]
+
+data['lcon'] = data.lcon.str.rstrip(')')
+data['lcon'] = data.lcon.str.lstrip()
+
+#clean LREC col by splitting apperances to date and record to date
+data[['lt_app2d','DROP','lt_rec2d']] = data.LREC.str.partition(',')
+data = data.drop(['LREC','DROP'],axis=1)
+data['lt_rec2d'] = data.lt_rec2d.str.rstrip(')')
+
+#clean winning goal scorer col
+data['win_gs'] = data.win_gs.str.rstrip()
+data[['win_gs','DROP','goal_time']] = data.win_gs.str.partition('(')
+data = data.drop('DROP',axis=1)
+data[['goal_time','DROP','period']] = data.goal_time.str.partition(', ')
+data = data.drop('DROP',axis=1)
+data['period'] = data.period.str.rstrip(')')
+
+data['game_end'] = np.where(data['period'] == 'first','Regulation',np.where(data['period'] == 'second','Regulation',np.where(data['period'] == 'third','Regulation','Overtime')))
+
+cols = list(data.columns.values)
+
+cols = ['Games','team_win', 'wcon', 'coach_win', 'win_gs',  'wt_app2d', 'wt_rec2d', 'team_lose', 'coach_lose', 'lcon', 'lt_app2d', 'lt_rec2d', 'goal_time', 'period', 'game_end']
+data = data[list(cols)]
+
+
+
+data['team_win'] = data.team_win.str.strip()
+data['team_lose'] = data.team_lose.str.strip()
+#print(data)
+data.replace({'team_win' : {'Chicago Black Hawks':'Chicago Blackhawks'}},inplace=True)
+data.replace({'team_lose' : {'Chicago Black Hawks':'Chicago Blackhawks'}},inplace=True)
+
+#create dictionaries of team appearances in cup final (win + lose)
+
+win_dic = Counter(data['team_win'])
+lose_dic = Counter(data['team_lose'])
+Total_dic = win_dic + lose_dic
+
+
+#create df from above dictionary
+total_cup_app = pd.DataFrame.from_dict(Total_dic, orient='index',columns=['Number of Cup Appearances'])
+total_cup_app = total_cup_app.sort_values('Number of Cup Appearances',ascending=False)
+total_cup_app.index.names = ['Team']
+#print(total_cup_app)
+
+#convert to time object and categorize time of goal
+data['goal_time'] = pd.to_datetime(data['goal_time'],format='%M:%S').dt.time
+temp = pd.DataFrame(data['goal_time'],columns=['goal_time'])
+
+temp[['goal_hour_DROP','goal_min','goal_sec']] = temp['goal_time'].astype(str).str.split(':', expand=True).astype(int)
+
+temp = temp.drop('goal_hour_DROP',axis=1)
+temp['gt_in_s'] = (temp['goal_min']*60) + temp['goal_sec']
+temp['gt_cat'] = np.where(temp['gt_in_s'] <= 300, 'First 5',np.where(temp['gt_in_s'] >= 900, 'Final 5', 'Middle 10'))
+data['gt_cat'] = temp['gt_cat']
+#print(data.head())
+
+#create dic like team but for coaches
+wc_dic = Counter(data['coach_win'])
+lc_dic = Counter(data['coach_lose'])
+coach_total_dic = wc_dic + lc_dic
+
+#create df from above dictionary
+total_coach_app = pd.DataFrame.from_dict(coach_total_dic, orient='index',columns=['Number of Cup Appearances'])
+total_coach_app = total_coach_app.sort_values('Number of Cup Appearances',ascending=False)
+total_coach_app.index.names = ['Coach']
+
+cols = list(data.columns.values)
+#print(cols) #to get list of column names to copy
+cols = ['Games', 'team_win', 'wcon', 'coach_win', 'win_gs', 'wt_app2d', 'wt_rec2d', 'team_lose', 'coach_lose', 'lcon', 'lt_app2d', 'lt_rec2d', 'goal_time', 'gt_cat', 'period', 'game_end']
+data = data[list(cols)]
+print(data.head())
+#print(data.win_gs.value_counts().head(10))
+
+plt.figure(num=1,figsize=(15,6))
+team_wins = data.team_win.value_counts(ascending=True).plot.barh()
+team_wins.set_xlabel('Frequency')
+team_wins.set_ylabel('Team')
+team_wins.set_title('Stanley Cup Finals Wins')
+team_wins.grid(True,which='major',axis='x')
+for i,v in enumerate(data.team_win.value_counts(ascending=True)):
+    team_wins.text(v,i," "+str(v), color='black',va='center',fontweight='bold')
+plt.tight_layout()
+#plt.savefig('team_wins.png')
+#plt.show()
+plt.close()
+
+plt.figure(num=2, figsize=(15,6))
+team_loses = data.team_lose.value_counts(ascending=True).plot.barh()
+team_loses.set_xlabel('Frequency')
+team_loses.set_ylabel('Team')
+team_loses.set_title('Number of Stanley Cup Finals Loses')
+team_loses.grid(True,which='major',axis='x')
+for i,v in enumerate(data.team_lose.value_counts(ascending=True)):
+    team_loses.text(v,i," "+str(v), color='black',va='center',fontweight='bold')
+plt.tight_layout()
+#plt.savefig('team_loses.png')
+#plt.show()
+plt.close()
+
+plt.figure(num=3,figsize=(15,10))
+coach_wins = data.coach_win.value_counts(ascending=True).plot.barh()
+coach_wins.set_xlabel('Frequency')
+coach_wins.set_ylabel('Coach')
+coach_wins.set_title('Number of Stanley Cup Finals Wins')
+coach_wins.grid(True,which='major',axis='x')
+for i,v in enumerate(data.coach_win.value_counts(ascending=True)):
+    coach_wins.text(v,i," "+str(v), color='black',va='center',fontweight='bold')
+#plt.yticks(rotation=45)
+plt.tight_layout()
+#plt.savefig('coach_wins.png')
+#plt.show()
+plt.close()
+
+plt.figure(num=4,figsize=(15,10))
+coach_loses = data.coach_lose.value_counts(ascending=True).plot.barh()
+coach_loses.set_xlabel('Frequency')
+coach_loses.set_ylabel('Coach')
+coach_loses.set_title('Number of Stanley Cup Finals Loses')
+coach_loses.grid(True,which='major',axis='x')
+for i,v in enumerate(data.coach_lose.value_counts(ascending=True)):
+    coach_loses.text(v,i," "+str(v), color='black',va='center',fontweight='bold')
+plt.tight_layout()
+#plt.savefig('coach_loses.png')
+#plt.show()
+plt.close()
+
+plt.figure(num=5)
+goal_timedis = data.gt_cat.value_counts(ascending=True).plot.bar()
+#plt.show()
+plt.close()
+
+plt.figure(num=6)
+goal_scorer = data.win_gs.value_counts(ascending=True).plot.barh()
+#plt.show()
+plt.close()
+
+plt.figure(num=7,figsize=(6,6))
+game_end = data.game_end.value_counts().plot.bar()
+for i,v in enumerate(data.game_end.value_counts()):
+    game_end.text(i,v + 1,str(v),color='black',ha='center',fontweight='bold')
+game_end.set_ylabel('Frequency')
+game_end.set_title('Frequency of Cup Winning Games\nEnding in OT')
+plt.xticks(rotation=0)
+plt.savefig('game_end.png')
+#plt.show()
+plt.close()
+
+
+
+plt.figure(num=8, figsize=(10,6))
+t5c = data.coach_win.value_counts().head()
+print(t5c)
+top5_coach = t5c.plot.bar(color='dodgerblue')
+plt.xticks(rotation=0)
+top5_coach.set_title('Top 5 Coaches by\nStanley Cup Final Wins',fontweight='bold')
+top5_coach.set_ylabel('Number of Cups')
+top5_coach.set_xlabel('Coach')
+for i,v in enumerate(t5c):
+    top5_coach.text(i,v+0.05,str(v),color='black',ha='center',fontweight='bold')
+plt.savefig('t5win_coach.png')
+plt.show()
+plt.close()
--- SCC1927-2019.py
+++ SCC1927-2019.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from collections import Counter
 data = pd.read_csv("C:/Users/chris/Documents/GitHub Repos/Stanley-Cup-Champs-1927-2019/_data/SCC1927-2019.csv",
                    header=0)
 
 # Pandas options for better viewing of data frame
 
-pd.set_option('max_rows', 10)  # set max rows printed to 10
+pd.set_option('max_rows', 100) # set max rows printed to 10
 pd.set_option('expand_frame_repr', False)  # expand frame by row width
-pd.set_option('large_repr', 'truncate')  # truncate data frame when rows > max rows above
+# truncate data frame when rows > max rows above
+pd.set_option('large_repr', 'truncate')
 
 print('Shape')
 print('=======================')
@@ -61,49 +61,65 @@ print(data.team_win)
 # winning team, winning team conference, winning team record in playoffs to date
 # we will need to split these into new columns
 
-data[['win_team', 'wt_conf', 'wt_rec2d']] = data.team_win.str.split("(", expand=True)
+data[['win_team', 'wt_conf', 'wt_rec2d']] = data.team_win.str.split(
+    "(", expand=True)
 # split team_win into 3 new cols,
 # need to clean wt_conf & wt_rec2d further
-data[['wt_cup_app2d', 'wt_cupfin_rec2d']] = data.wt_rec2d.str.split(",", expand=True)
+data[['wt_cup_app2d', 'wt_cupfin_rec2d']
+] = data.wt_rec2d.str.split(",", expand=True)
 # split cup final appearances from cup final record of winning team
-data['wt_cupfin_rec2d'] = data['wt_cupfin_rec2d'].map(lambda x: str(x)[:-1])  # drops unwanted ) from cup final records
-data['wt_conf'] = data['wt_conf'].map(lambda x: str(x)[:-2])  # used -2 as there was a space after the )
-
+data['wt_cupfin_rec2d'] = data['wt_cupfin_rec2d'].map(
+    lambda x: str(x)[:-1])  # drops unwanted ) from cup final records
+# used -2 as there was a space after the )
+data['wt_conf'] = data['wt_conf'].map(lambda x: str(x)[:-2])
+data['win_team'] = data['win_team'].map(lambda x: str(x)[:-1])
 # now that winning team cols are cleaned, we drop the unwanted cols before cleaning losing team cols
 
-data = data.drop(columns=['team_win','wt_rec2d'])
+data = data.drop(columns=['team_win', 'wt_rec2d'])
 
-data[['lose_team', 'lt_conf', 'lt_rec2d']] = data.team_lose.str.split("(", expand=True)
-data[['lt_cup_app2d', 'lt_cupfin_rec2d']] = data.lt_rec2d.str.split(",", expand=True)
+data[['lose_team', 'lt_conf', 'lt_rec2d']
+] = data.team_lose.str.split("(", expand=True)
+data[['lt_cup_app2d', 'lt_cupfin_rec2d']
+] = data.lt_rec2d.str.split(",", expand=True)
 data['lt_cupfin_rec2d'] = data['lt_cupfin_rec2d'].map(lambda x: str(x)[:-1])
 data['lt_conf'] = data['lt_conf'].map(lambda x: str(x)[:-2])
+data['lose_team'] = data['lose_team'].map(lambda x: str(x)[:-1])
+data = data.drop(columns=['team_lose', 'lt_rec2d'])
 
-data = data.drop(columns=['team_lose','lt_rec2d'])
+# now we need to clean the winning goal-scorer, for simplicity we only care about who scored the goal, not when
 
-#now we need to clean the winning goal-scorer, for simplicity we only care about who scored the goal, not when
-
-data[['win_goal_scorer','DROP']] = data.win_gs.str.split("(", expand=True)
+data[['win_goal_scorer', 'DROP']] = data.win_gs.str.split("(", expand=True)
 data['win_goal_scorer'] = data['win_goal_scorer'].map(lambda x: str(x)[:-1])
-data = data.drop(columns=['win_gs','DROP'])
+data = data.drop(columns=['win_gs', 'DROP'])
 
 print(data)
 print(data.info(
 ))
 
 print(data.win_team.value_counts())
-data.win_team.value_counts().plot(kind='bar')
-data.lose_team.value_counts().plot(kind='bar')
+
 print(data.wt_conf.value_counts())
 
-#since conferences have been changed so much over the years it does not provide much info so we will drop
-# cup appearances and record will also be dropped for simplicity
+# since conferences have been changed so much over the years it does not provide much info so we will drop
 
-data = data.drop(columns=['wt_conf','wt_cup_app2d','wt_cupfin_rec2d','lt_conf','lt_cup_app2d','lt_cupfin_rec2d'])
-print(data
+
+data_cleaned = data.drop(columns=['wt_conf', 'wt_cup_app2d',
+                                     'lt_conf', 'lt_cup_app2d'])
+print(data_cleaned
       )
-cols = list(data.columns.values)
+cols = list(data_cleaned.columns.values)
 print(cols)
-cols = ['Games','win_team','coach_win','win_goal_scorer','lose_team', 'coach_lose']
-data = data[['Games','win_team','coach_win','win_goal_scorer','lose_team', 'coach_lose']]
-print(data)
-data.to_excel('SCC1927-2019-clean.xlsx')
+
+data_cleaned = data_cleaned[['Games', 'win_team', 'coach_win',
+                        'win_goal_scorer','wt_cupfin_rec2d','lose_team', 'coach_lose','lt_cupfin_rec2d']]
+print(data_cleaned)
+#ata_cleaned.to_excel('SCC1927-2019-clean.xlsx')
+win_dic = Counter(data_cleaned['win_team'])
+lose_dic = Counter(data_cleaned['lose_team'])
+print(win_dic)
+print(lose_dic)
+Total_apps = win_dic + lose_dic
+print(Total_apps)
+Total_Cup_App_by_team = pd.DataFrame.from_dict(Total_apps, orient='index',columns=['Number of Cup Appearances'])
+Total_Cup_App_by_team = Total_Cup_App_by_team.sort_values('Number of Cup Appearances',ascending=False)
+print(Total_Cup_App_by_team)
